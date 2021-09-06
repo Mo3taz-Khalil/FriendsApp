b94a0ef4386ce40f7604bc150ed149bdf9c4845c
Moataz Mohamed <moataz.mohamed.sd@gmail.com>
2021-09-06 12:10:02 +0200

    End of section 9

---
+++ client/src/app/_interceptors/jwt.interceptor.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { AccountService } from '../_services/account.service';
+import { User } from '../_models/user';
+import { take } from 'rxjs/operators';
+
+@Injectable()
+export class JWTInterceptor implements HttpInterceptor {
+
+  constructor(private accountservice: AccountService) { }
+
+  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+
+    let currentUser: User | null = { username: '', token: '' };
+
+    //we want to complete after recieve one current user --take-- so after it complete we are not subscribe to it anymore 
+    this.accountservice.currentUser$.pipe(take(1)).subscribe(user => {
+      if (user) {
+        currentUser = user;
+      } else currentUser = null;
+    });
+    if (currentUser) {
+      request = request.clone({
+        setHeaders: {
+          Authorization: `Bearer ${currentUser.token}`
+        }
+      })
+    }
+
+    return next.handle(request);
+  }
+}
--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -19,6 +19,7 @@ export class AppComponent implements OnInit {
   }
 
   setCurrentUser() {
+    
     const user: User = JSON.parse(localStorage.getItem('user') || '{}');
     if (user.username != null) {
       this.accountService.setCurrentUser(user);
